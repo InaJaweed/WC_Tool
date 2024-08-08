829dad5fd87bd59122af54a8225d6128ae4bb9f6
Jaweed Ina <github.ligament060@passinbox.com>
2024-08-08 16:29:59 +0100

    commented code, created readme

---
+++ README.md
@@ -0,0 +1,19 @@
+compare arguments - https://stackoverflow.com/questions/27525617/how-to-properly-compare-command-line-arguments#:~:text=To%20compare%20strings%2C%20you%20need%20to%20use%20strcmp()%20.&text=if%20you%20want%20to%20check,%22%2Ds%22%20or%20not.&text=Compare%20the%20two%20strings%20using,(s1%2Cs2)%20function.&text=if%20you%20check%20the%20argv,condition%20will%20not%20be%20true.
+
+file size - https://stackoverflow.com/questions/48063652/how-can-i-get-a-files-size-in-c-without-using-either-fseek-or-stat
+https://ccodelearner.com/c-examples/find-size-of-file/#:~:text=Finding%20the%20Size,-Once%20we%20have&text=In%20C%2C%20we%20can%20use%20the%20fseek()%20and%20ftell,the%20next%20operation%20will%20occur.
+
+line count - https://stackoverflow.com/questions/12733105/c-function-that-counts-lines-in-file
+https://www.youtube.com/watch?v=AAF207cxbXk
+
+https://codeforwin.org/c-programming/c-program-count-characters-words-lines-in-file
+
+Word count - https://www.javatpoint.com/program-to-find-the-number-of-words-in-the-given-text-file
+https://stackoverflow.com/questions/7374062/how-do-i-count-the-number-of-words-in-a-text-file-using-c
+
+
+Future
+
+- Handle buffer overunrs
+- Ensure there are enough arguments before accessing argv[2].
+- Provide clear usage instructions if arguments are missing or invalid.
--- ccwc.c
+++ ccwc.c
@@ -7,153 +7,165 @@
 
 #define BUFF_SIZE 4096
 
-// Function to compare string
-int strcompare(const char *str1, const char *str2){
-    while(*str1 != '\0' && *str2 != '\0'){
-        if(*str1 != *str2){
-            return 0;
+// Function to compare two strings for equality
+int strcompare(const char *str1, const char *str2) {
+    while(*str1 != '\0' && *str2 != '\0') {
+        if(*str1 != *str2) {
+            return 0; // Strings are not equal
         }
         str1++;
         str2++;
     }
+    // Return 1 if both strings ended simultaneously, indicating equality
     return (*str1 == '\0' && *str2 == '\0');
 }
 
-// Function to find bytes size of file
-long getFileSize(FILE *file) {
+// Function to calculate the size of a file
+long int getFileSize(FILE *file) {
     long size = 0;
     char buffer[BUFF_SIZE];
     size_t bytesRead;
 
-    // Read the file in chunks of 4096 bytes (4 Kilo Bytes)
+    // Read the file in chunks of 4096 bytes
     while((bytesRead = fread(buffer, 1, BUFF_SIZE, file)) > 0) {
-        size += bytesRead;
+        size += bytesRead; // Accumulate the total bytes read
     }
 
-    if(ferror(file)){
-        perror("Error reading file");
+    if(ferror(file)) {
+        perror("Error reading file"); // Print error if reading fails
         return -1;
     }
 
-    fclose(file); // Close the file
+    fclose(file); // Close the file after reading
     return size;
 }
 
-size_t getLineCount(FILE *file){
+// Function to count the number of lines in a file
+size_t getLineCount(FILE *file) {
     char buffer[BUFF_SIZE];
     size_t bytesRead;
     size_t lineCount = 0;
 
-    while((bytesRead = fread(buffer, 1, BUFF_SIZE, file)) > 0){
-        for(size_t i = 0; i < bytesRead; i++){
-            if (buffer[i] == '\n'){
-                lineCount++;
+    // Read the file in chunks of 4096 bytes
+    while((bytesRead = fread(buffer, 1, BUFF_SIZE, file)) > 0) {
+        for(size_t i = 0; i < bytesRead; i++) {
+            if (buffer[i] == '\n') {
+                lineCount++; // Increment line count for each newline character
             }
         }
     }
 
-    if(ferror(file)){
-        perror("Error reading file");
-        return (size_t) - 1;
+    if(ferror(file)) {
+        perror("Error reading file"); // Print error if reading fails
+        return (size_t) -1;
     }
 
-    if(bytesRead == 0 && buffer[BUFF_SIZE - 1] != '\n'){
+    // If the last chunk does not end with a newline, count the last line
+    if(bytesRead == 0 && buffer[BUFF_SIZE - 1] != '\n') {
         lineCount++;
     }
 
-    fclose(file);
+    fclose(file); // Close the file after reading
     return lineCount;
 }
 
-size_t getWordCount(FILE *file){
+// Function to count the number of words in a file
+size_t getWordCount(FILE *file) {
     char buffer[BUFF_SIZE];
     size_t bytesRead;
     size_t wordCount = 0;
-    size_t notword = 0;
+    size_t notword = 0; // Flag to indicate if the last character was not a word character
 
-    while((bytesRead = fread(buffer, 1, BUFF_SIZE, file)) > 0){
-        for(size_t i = 0; i < bytesRead; i++){
-            if(isspace((unsigned char)buffer[i])){
-                notword = 0;
-            }else{
-                if(!notword){
-                    wordCount++;
-                    notword = 1;
+    // Read the file in chunks of 4096 bytes
+    while((bytesRead = fread(buffer, 1, BUFF_SIZE, file)) > 0) {
+        for(size_t i = 0; i < bytesRead; i++) {
+            if(isspace((unsigned char)buffer[i])) {
+                notword = 0; // Reset the flag when a whitespace is found
+            } else {
+                if(!notword) {
+                    wordCount++; // Increment word count when a new word is encountered
+                    notword = 1; // Set the flag to indicate that we are in a word
                 }
             }
         }
     }
 
-    if(ferror(file)){
-        perror("Error reading file");
-        return (size_t) - 1;
+    if(ferror(file)) {
+        perror("Error reading file"); // Print error if reading fails
+        return (size_t) -1;
     }
 
-    fclose(file);
+    fclose(file); // Close the file after reading
     return wordCount;
 }
 
-int main(int argc, char *argv[]){
-    if(argc < 2){
-        printf("usage %s <filename>\n", argv[0]);
+int main(int argc, char *argv[]) {
+    // Check if sufficient arguments are provided
+    if(argc < 3) {
+        printf("usage %s <option> <filename>\n", argv[0]);
         return -1;
     }
 
-    FILE *file = fopen(argv[2], "rb"); // Open the file in binary mode
+    // Open the specified file in read mode
+    FILE *file = fopen(argv[2], "r");
     if (file == NULL) {
-        perror("Unable to open file");
+        perror("Unable to open file"); // Print error if file cannot be opened
         return -1;
     }
 
-    if(strcompare(argv[1],"-c") == 1){
-        // reopen the file to reset the file pointer
+    // Process the command-line option
+    if(strcompare(argv[1], "-c") == 1) {
+        // Reopen the file to reset the file pointer
         fclose(file);
         file = fopen(argv[2], "r");
-        if(file == NULL){
+        if(file == NULL) {
             perror("Unable to open file");
             return -1;
         }
 
-        long size = getFileSize(file);
+        long size = getFileSize(file); // Get the size of the file
 
-        if(size != -1){
+        if(size != -1) {
             printf("File: %s\nSize: %ld\n", argv[2], size);
-        }else{
+        } else {
             perror("Error");
         }
     }
-    
-    else if (strcompare(argv[1], "-l") == 1){
-        // reopen the file to reset the file pointer
+    // Line count option
+    else if (strcompare(argv[1], "-l") == 1) {
+        // Reopen the file to reset the file pointer
         fclose(file);
         file = fopen(argv[2], "r");
-        if(file == NULL){
+        if(file == NULL) {
             perror("Unable to open file");
             return -1;
         }
 
-        size_t lineCount = getLineCount(file);
+        size_t lineCount = getLineCount(file); // Get the number of lines
+
         if (lineCount == (size_t)-1) {
             return -1; // Error was handled in getLineCount
         }
         printf("File: %s\nLine count: %zu\n", argv[2], lineCount);
     }
-
-    else if(strcompare(argv[1], "-w") == 1){
-        // reopen the file to reset the file pointer
+    // Word count option
+    else if(strcompare(argv[1], "-w") == 1) {
+        // Reopen the file to reset the file pointer
         fclose(file);
         file = fopen(argv[2], "r");
-        if(file == NULL){
+        if(file == NULL) {
             perror("Unable to open file");
             return -1;
         }
 
-        size_t wordCount = getWordCount(file);
-        if(wordCount == (size_t) - 1){
+        size_t wordCount = getWordCount(file); // Get the number of words
+
+        if(wordCount == (size_t) -1) {
             return -1;
         }
         printf("File: %s\n%zu\n", argv[2], wordCount);
     }
-    fclose(file);
+    
+    fclose(file); // Close the file before exiting
     return 0;
 }
