c19d8b290d9e331aa7eb7c96aa3bb9f18703c759
Jaweed Ina <github.ligament060@passinbox.com>
2024-08-08 13:58:16 +0100

    created my own strin compare to compare the arguments, get line count and output line count to user

--- ccwc.c
+++ ccwc.c
@@ -4,6 +4,20 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define BUFF_SIZE 4096
+
+// Function to compare string
+int strcompare(const char *str1, const char *str2){
+    while(*str1 != '\0' && *str2 != '\0'){
+        if(*str1 != *str2){
+            return 0;
+        }
+        str1++;
+        str2++;
+    }
+    return (*str1 == '\0' && *str2 == '\0');
+}
+
 // Function to find bytes size of file
 long getFileSize(const char *filename) {
     long size = 0;
@@ -14,16 +28,43 @@ long getFileSize(const char *filename) {
         return -1;
     }
 
-    char buffer[4096];
+    char buffer[BUFF_SIZE];
     size_t bytesRead;
 
     // Read the file in chunks of 4096 bytes (4 Kilo Bytes)
-    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+    while((bytesRead = fread(buffer, 1, BUFF_SIZE, file)) > 0) {
         size += bytesRead;
     }
 
     fclose(file); // Close the file
     return size;
+}
+
+size_t getLineCount(const char *filename){
+    FILE *file = fopen(filename, "r"); // Open file in read mode
+    if(file == NULL){
+        perror("Unable to open file");
+        return -1;
+    }
+
+    char buffer[BUFF_SIZE];
+    size_t bytesRead;
+    size_t lineCount = 0;
+
+    while((bytesRead = fread(buffer, 1, BUFF_SIZE, file)) > 0){
+        for(size_t i = 0; i < bytesRead; i++){
+            if (buffer[i] == '\n'){
+                lineCount++;
+            }
+        }
+    }
+
+    if(bytesRead == 0 && buffer[bytesRead - 1] != '\n'){
+        lineCount++;
+    }
+
+    fclose(file);
+    return lineCount;
 }
 
 int main(int argc, char *argv[]){
@@ -32,19 +73,30 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    FILE *file = fopen(argv[1], "rb"); // Open the file in binary mode
+    FILE *file = fopen(argv[2], "rb"); // Open the file in binary mode
     if (file == NULL) {
         perror("Unable to open file");
         return -1;
     }
- 
-    long size = getFileSize(argv[1]);
 
-    if(size != -1){
-        printf("Size = %ld\n", size);
-    }else{
-        printf("Error");
+    if(strcompare(argv[1],"-c") == 1){
+        long size = getFileSize(argv[2]);
+
+        if(size != -1){
+            printf("File: %s\nSize = %ld\n",argv[2], size);
+        }else{
+            perror("Error");
+        }
+    }
+    
+    else if (strcompare(argv[1], "-l") == 1)
+    {
+        size_t lineCount = getLineCount(argv[2]);
+        if (lineCount == (size_t)-1) {
+            return -1; // Error was handled in getLineCount
     }
 
+        printf("File: %s\nLine count: %zu\n",argv[2], lineCount);
+    }
     return 0;
 }
